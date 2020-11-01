523a7b9cfc12842350853fc074ea4a0f37770e05
Corleone02 <39175488+Natan0202@users.noreply.github.com>
2020-10-31 22:07:20 -0300

    Boneco_Stick-Se-Movendo
    By: Natan0202
    Desenhos em pixel, JS aplicado, troca de fundo ao clicar com o botão. Se divirta!

---
+++ clique.js
@@ -0,0 +1,10 @@
+function clicar(){
+    document.getElementById("img").src = "pixil-frame-0-1.png";
+}
+function voltar(){
+    document.getElementById("img").src = "pixil-frame-0-0.png";
+}
+function cuidado(){
+    document.getElementById("img").src = "pixil-frame-0-2.png";
+
+}
